01d214a283a538412e394995191be6d9cb2f0c3f
Jimmy Gouraud <jimmy.gouraud@hotmail.fr>
2017-10-14 18:42:31 +0200

    Finish TD3

---
+++ TD3/main.m
@@ -0,0 +1,8 @@
+
+filenameA = 'a.png';
+filenameB = 'b.png';
+filenameC = 'c.png';
+patch_hs = 4;
+nb_iter = 4;
+
+patchMatching(nb_iter, patch_hs, filenameA, filenameB, filenameC);
--- TD3/patchMatching.m
+++ TD3/patchMatching.m
@@ -1,80 +1,111 @@
-
-imgA = im2double(imread('a.png'));
-imgB = im2double(imread('b.png'));
-imgC = zeros(size(imgA, 1), size(imgA, 2), 3);
-
-patch_hs = 4;
-patch_size = patch_hs * 2 + 1;
-coor = zeros(size(imgC,1),size(imgC,2),2);
-
-pad_imgA  = padarray(imgA,  [patch_hs, patch_hs]);
-pad_imgB = padarray(imgB, [patch_hs, patch_hs]);
-
-% Initialisation de C avec des pixels aléatoires de B
-index = 1
-for i=1:size(imgC,1)
-    for j=1:size(imgC,2)
-        i2 = randi(size(imgC,1)-patch_size)+patch_hs;
-        j2 = randi(size(imgC,2)-patch_size)+patch_hs;
-        imgC(i,j,:) = imgB(i2,j2,:);
-        coor(i,j,:) = [i2,j2];
-        index = index+1;
+function [ imgC ] = patchMatching( nb_iter, patch_hs, filenameA, filenameB, filenameC )
+    if (exist ('OCTAVE_VERSION', 'builtin'))
+      pkg load image
     end
-end
 
-subplot(1,3,1);
-imagesc(imgA);
-title('imgA');
-subplot(1,3,2);
-imagesc(imgB);
-title('imgB');
-subplot(1,3,3);
-imagesc(imgC);
-title('imgC');
+    imgA = im2double(imread(filenameA));
+    imgB = im2double(imread(filenameB));
+    imgC = zeros(size(imgA, 1), size(imgA, 2), 3);
+          
+    patch_size = patch_hs * 2 + 1;
+    coor = zeros(size(imgC,1),size(imgC,2),2);
 
-figure;
-for i=1:size(imgC,1)
-    for j=1:size(imgC,2)
-        i2 = coor(i,j,1);
-        j2 = coor(i,j,2);
-        patch_imgA = pad_imgA(i:i+patch_size-1, j:j+patch_size-1,:);
-        patch_imgB = pad_imgB(i2:i2+patch_size-1, j2:j2+patch_size-1,:);
-        
-        best_i = i2;
-        best_j = j2;
-        
-        ssd = computeSSD(patch_imgA, patch_imgB);
-        if (i > 1 && i2 < size(pad_imgB,1)-patch_hs)
-            i2 = coor(i-1,j,1);
-            j2 = coor(i-1,j,2);
-            patch_imgB = pad_imgB(i2+1:i2+1+patch_size-1, j2:j2+patch_size-1,:);
-            ssd2 = computeSSD(patch_imgA, patch_imgB);
-            if (ssd2 < ssd) 
-               best_i = i2;
-               best_j = j2;
-               ssd = ssd2;
-            end
+    pad_imgA  = padarray(imgA,  [patch_hs, patch_hs]);
+    pad_imgB = padarray(imgB, [patch_hs, patch_hs]);
+
+    % Initialisation de C avec des pixels aléatoires de B
+    for i=1:size(imgC,1)
+        for j=1:size(imgC,2)
+            i2 = randi(size(imgC,1)-patch_size)+patch_hs;
+            j2 = randi(size(imgC,2)-patch_size)+patch_hs;
+            imgC(i,j,:) = imgB(i2,j2,:);
+            coor(i,j,:) = [i2,j2];
         end
-        
-        if (j > 1 && j2 < size(pad_imgB,1)-patch_hs)
-            i2 = coor(i,j-1,1);
-            j2 = coor(i,j-1,2);
-            patch_imgB = pad_imgB(i2:i2+patch_size-1, j2+1:j2+patch_size,:);
-            ssd2 = computeSSD(patch_imgA, patch_imgB);
-            if (ssd2 < ssd) 
-               best_i = i2;
-               best_j = j2;
-               ssd = ssd2;
-            end
-        end
-        
-        imgC(i,j,:) = imgB(best_i, best_j,:);
-        index = index+1;
     end
-    i
+
+    figure;
+    subplot(1,3,1);
+    imagesc(imgA);
+    title('imgA');
+    subplot(1,3,2);
+    imagesc(imgB);
+    title('imgB');
+    subplot(1,3,3);
     imagesc(imgC);
-    drawnow;
+    title('imgC');
+
+    figure;
+    for iter=1:nb_iter
+      iter
+      
+      if (mod(iter,2) == 1)
+         di = -1;
+         dj = -1;
+      else
+         di = 1;
+         dj = 1;
+      end
+      
+      for i=1:size(imgC,1)
+          for j=1:size(imgC,2)
+              if (mod(iter,2) == 1)
+                  i1 = i;
+                  j1 = j;
+              else
+                  i1 = size(imgC,1)-i+1;
+                  j1 = size(imgC,2)-j+1;
+              end
+              
+              i2 = coor(i1,j1,1);
+              j2 = coor(i1,j1,2);
+              patch_imgA = pad_imgA(i1:i1+patch_size-1, j1:j1+patch_size-1,:);
+              patch_imgB = pad_imgB(i2:i2+patch_size-1, j2:j2+patch_size-1,:);
+              
+              best_i = i2;
+              best_j = j2;
+              
+              ssd = computeSSD(patch_imgA, patch_imgB);
+              if (i1+di >= 1 && i1+di <= size(imgC,1))
+                  i2 = coor(i1+di,j1,1)-di;
+                  j2 = coor(i1+di,j1,2);
+                  if(i2 < 1 || i2 > size(imgB,1)) 
+                    continue;
+                  end
+                  
+                  patch_imgB = pad_imgB(i2:i2+patch_size-1, j2:j2+patch_size-1,:);
+                  ssd2 = computeSSD(patch_imgA, patch_imgB);
+                  if (ssd2 < ssd) 
+                     best_i = i2;
+                     best_j = j2;
+                     ssd = ssd2;
+                  end
+              end
+              
+              if (j1+dj >= 1 && j1+dj <= size(imgC,2))
+                  i2 = coor(i1,j1+dj,1);
+                  j2 = coor(i1,j1+dj,2)-dj;
+                  if(i2 < 1 || i2 > size(imgB,2)) 
+                    continue;
+                  end
+                  
+                  patch_imgB = pad_imgB(i2:i2+patch_size-1, j2:j2+patch_size-1,:);
+                  ssd2 = computeSSD(patch_imgA, patch_imgB);
+                  if (ssd2 < ssd) 
+                     best_i = i2;
+                     best_j = j2;
+                     ssd = ssd2;
+                  end
+              end
+              
+              imgC(i1,j1,:) = imgB(best_i, best_j,:);
+              coor(i1,j1,:) = [best_i,best_j];
+          end
+          imagesc(imgC);
+          drawnow;
+      end
+      filenameIter = strcat("iter", int2str(iter), ".png");
+      imwrite (imgC, filenameIter);
+    end
+    
+    imwrite (imgC, filenameC);
 end
-
-
-
